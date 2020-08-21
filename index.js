28579b36a9c7b05aafd6be12eee850b66b960bff
Moral <jeramoral@gmail.com>
2020-08-21 11:52:25 +0000

    added js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var startButton = document.getElementById('start');
-var sound = document.getElementById('sound');
-var bgm =document.getElementById('bgm');
+function play(){
+var  sound = new Audio('blob:https://www.youtube.com/c8f0551c-c702-4075-83b5-e38331ef92f7');
+audio.play();
+var  sound = new Audio('blob:https://www.youtube.com/51393357-7cb7-4490-b729-36c6e6b1249b');
+audio.play();
+var  sound = new Audio('blob:https://www.youtube.com/dd6978e1-e943-46b0-a483-5974b7c80ce0');
+audio.play();
+var  sound = new Audio('blob:https://www.youtube.com/c115313c-ece6-43ab-a952-697f649dc842');
+audio.play();
+var  sound = new Audio('blob:https://www.youtube.com/1a9ac3c4-65fe-496f-b345-c56957fe22d3');
+audio.play();
+};
+<button onclick = 'play();'> Play animals sound!</button>
